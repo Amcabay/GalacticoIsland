773d9e7060b5a7660cd2b3015d8cbd5d57ff6682
Amcabay <amca.raftan@gmail.com>
2022-11-15 15:01:31 +0700

    Final 1.0

---
+++ Assets/Assets/Script/MiniGames_1/ExitMini.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitMini : MonoBehaviour
+{
+    public int sceneBuildIndex;
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            print("Switching Scene to " + sceneBuildIndex);
+            SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
+        }
+    } 
+
+}
+
