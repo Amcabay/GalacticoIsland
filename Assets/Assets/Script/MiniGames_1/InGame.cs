edfa6dda3ebe9e2e898e67ac46c844d7868b7b8a
Amcabay <amca.raftan@gmail.com>
2022-11-17 21:53:02 +0700

    Score

---
+++ Assets/Assets/Script/MiniGames_1/InGame.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGame : MonoBehaviour
+{
+    public GameObject[] kaleng;
+    float i;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        i += Time.deltaTime;
+        if(i > 1f)
+        {
+            //Duplikasi kaleng baru
+            GameObject kalengBaru = Instantiate(kaleng[Random.Range(0, 1)]);
+            i = 0;
+        }
+
+        //Menembak
+        if(Input.GetMouseButtonDown(0))
+        {
+           
+        }
+    }
+}
